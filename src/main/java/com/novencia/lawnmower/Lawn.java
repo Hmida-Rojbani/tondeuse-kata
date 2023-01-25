0b459ae07989db6ddd500fb5b9a8fe0434d785b9
Hmida-Rojbani <hmida.rojbani@fst.utm.tn>
2023-01-25 06:13:14 +0100

    Add documentation to Lawn and LawnMower

--- src/main/java/com/novencia/lawnmower/Lawn.java
+++ src/main/java/com/novencia/lawnmower/Lawn.java
@@ -1,6 +1,11 @@
 package com.novencia.lawnmower;
 
 public record Lawn(Coordinate upperRightCorner, Coordinate lowerLeftCorner) {
+    /**
+     * Test if the coordinate is inside the space limit of the lawn
+     * @param coordinate to test
+     * @return true if the coordinate is inside the lawn
+     */
     public boolean isInside(Coordinate coordinate){
         return (coordinate.x()>= lowerLeftCorner.x() && coordinate.x() <= upperRightCorner().x())
                 &&
--- src/main/java/com/novencia/lawnmower/LawnMower.java
+++ src/main/java/com/novencia/lawnmower/LawnMower.java
@@ -1,5 +1,8 @@
 package com.novencia.lawnmower;
 
+/**
+ *
+ */
 public class LawnMower {
     private Position position;
     private Lawn lawn;
@@ -9,12 +12,20 @@ public class LawnMower {
         this.lawn = lawn;
     }
 
+    /**
+     * Execute a sequence of instructions
+     * @param instructions
+     */
     public void execute(String instructions) {
         instructions.chars()
                 .mapToObj( c -> (char) c)
                 .forEach(this::execute);
     }
 
+    /**
+     * Execute single Instruction
+     * @param instruction a character must one of this set {'D','G','A'}
+     */
     private void execute(Character instruction) {
         switch (instruction) {
             case 'D' -> rotateToRight();
@@ -24,16 +35,24 @@ public class LawnMower {
         }
     }
 
+    /**
+     * Advance lawnmower forward if is possible
+     */
     private void advance() {
         Position nextPosition = position.moveForward();
         if(lawn.isInside(nextPosition.coordinate()))
             position = nextPosition;
     }
 
+    /**
+     * Rotate lawnmower to the left
+     */
     private void rotateToLeft() {
         position = position.rotateToLeft();
     }
-
+    /**
+     * Rotate lawnmower to the right
+     */
     private void rotateToRight() {
         position = position.rotateToRight();
     }
